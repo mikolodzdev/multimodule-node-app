89c1b7a0947f31f11368df9c608f6b52f261986f
miko <mk.dev@outlook.com>
2018-05-01 17:06:00 +0200

    remove JS files

--- common/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-function toUpperCase(str) {
-    return str.toUpperCase();
-}
-exports.default = {
-    toUpperCase: toUpperCase
-};
-//# sourceMappingURL=index.js.map
--- config/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.env = {
-    server: {
-        PORT: 3000
-    }
-};
-//# sourceMappingURL=env.js.map
